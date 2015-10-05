1fba21bf4b1d03b8fd7e362eec068e3b9dfd6d5c
Neil Munro <neilmunro@gmail.com>
2015-10-05 09:21:02 +0100

    Added the actual tests... Opps.

---
+++ tests.js
@@ -0,0 +1,23 @@
+QUnit.test("Any function", function(assert) {
+  assert.ok(1 == 1, "Ok");
+});
+
+QUnit.test("All function", function(assert) {
+  assert.ok(1 == 1, "Ok");
+});
+
+QUnit.test("First function", function(assert) {
+  assert.ok(1 == 1, "Ok");
+});
+
+QUnit.test("Nth function", function(assert) {
+  assert.ok(1 == 1, "Ok");
+});
+
+QUnit.test("Last function", function(assert) {
+  assert.ok(1 == 1, "Ok");
+});
+
+QUnit.test("Rest function", function(assert) {
+  assert.ok(1 == 1, "Ok");
+});
